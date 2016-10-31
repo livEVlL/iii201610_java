7c0c09c243ffd8ac39ccc1501a6f770ab6f94412
Arsene <livevil404@gmail.com>
2016-10-31 11:59:30 +0800

    2016-10/31-testGitHub

---
+++ src/tw/org/iii/javatest/Hello.java
@@ -0,0 +1,11 @@
+package tw.org.iii.javatest;
+
+/**
+ * Created by iii-user on 2016/10/31.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello,World");
+       // System.exit(0);
+    }
+}
