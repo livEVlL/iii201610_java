f9ad6c24f864b698447a4d0fdfdf942f6c498575
flysummer <t0003406@hotmail.com>
2016-11-10 16:31:00 +0800

    2016-11/10-test

---
+++ src/tw/org/iii/javatest/Fibonacci.java
@@ -0,0 +1,9 @@
+package tw.org.iii.javatest;
+public class Fibonacci {
+    public static void main(String[] args) {
+        int[] number = {70,80,31,37,10,1,48,60,33,81};
+
+        System.out.print(number);
+    }
+}
+
--- src/tw/org/iii/javatest/Java23.java
+++ src/tw/org/iii/javatest/Java23.java
@@ -2,7 +2,7 @@ package tw.org.iii.javatest;
 
 public class Java23 {
     public  static  void main(String[] args){
-        TWId id1 = new TWId();
+        TWId id1 = new TWId("A123456789");
         System.out.println(id1.getId());
         if (TWId.isCheckOK(id1.getId())){
             System.out.println("OK");
